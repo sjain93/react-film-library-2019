0faa76a63e55e447aa164a864f0f112ec35ec2fa
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-04-09 00:02:41 -0400

    Completed section 9, added filmposter component

---
+++ src/FilmPoster.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FilmPoster = ({film}) => {
+    return (
+        <img src = {` https://image.tmdb.org/t/p/w780/${film.poster_path}`} alt={film.title}></img>
+    );
+};
+
+export default FilmPoster;
